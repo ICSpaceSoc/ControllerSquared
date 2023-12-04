f385ac3027893ca7e54f8f47dfa3e2dcf9dfb150
nadav7679 <nadav76799@gmail.com>
2023-12-04 22:20:44 +0000

    PID minor bugfix

--- onboard/structures/pid.py
+++ onboard/structures/pid.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy as sp
 
 class PID:
 
@@ -19,54 +18,66 @@ class PID:
         self.ki = ki
         self.kd = kd
         self.integral = 0
-        self.most_recent_timestep_cached = 0 # the most recent timstep included in the integral.
+        self.most_recent_index_cached = 0 # the most recent timstep included in the integral.
 
     def update(self, current_time) -> float:
         """_summary_
 
         Args:
-            current_time (_type_): _description_
+            current_time (_type_): current time in index
 
         Returns:
             float: _description_
         """
 
         y = self.buffer[current_time, 'timestamp']['corr_value'] #current value. TODO: adjust this to access the buffer properly
+        y = self.buffer[current_time][1] #current value. TODO: adjust this to access the buffer properly
+        print(y)
         e = self.setpoint - y
-        data = self.buffer[self.most_recent_timestep_cached: current_time, "timestamp"]
+        data = self.buffer[self.most_recent_index_cached: current_time]
 
         # data = [
-        #     (value, smoothvalue, self.most_recent_timestep_cached)
+        #     (value, smoothvalue, self.most_recent_index_cached)
         #     (value, smoothvalue, t)
         #     ...
         #     (value, smoothvalue, current_time)
         # ]
 
         for i in range(len(data)-1): 
-            time_diff = data[i+1][2]-data[i][2]
+            time_diff = (data[i+1][2]-data[i][2]).total_seconds()
+            
             value_sum = data[i+1][1]-data[i][1]
             self.integral += time_diff*0.5*value_sum
         
 
         P = self.kp * e
-        I = self.ki *  self.self.integral
-        D = self.self.kd * (data[-1][1]-data[-2][1])/(data[-1][2]-data[-2][2])
+        I = self.ki *  self.integral
+        D = self.kd * (data[-1][1]-data[-2][1])/(data[-1][2]-data[-2][2]).total_seconds()
         # we can always change how this grad is computed (ie: number of points to lookback)
         u = P + I + D
+        
         return u
 
 
 def test():
     from Buffer import Buffer
-    import matplotlib.pyplot as plt
+    from Buffer import Buffer
     import datetime as dt
-
-    buff = Buffer(10)
-    for i in range(15):
-        buff += (i, i, 0.1 * i)
+    import matplotlib.pyplot as plt
+    import time
+    
+    BUFF_MAX = 100
+    
+    
+    buff = Buffer(BUFF_MAX, [('value', np.float64), ('corr_value', np.float64), ('timestamp', dt.datetime)])
+    for i in range(BUFF_MAX):
+        time.sleep(0.1)
+        buff += (i, i, dt.datetime.now())
+        
+        
 
     pid = PID(5, buff, 1, 2, 3)
-    print(pid.update(dt.datetime.now()))
+    print(pid.update(BUFF_MAX - 1))
 
 
 if __name__ == "__main__":  # only runs when this file is run directly in terminal; if imported to somewhere else this doesn't run
